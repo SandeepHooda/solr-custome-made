170199823a374bb241472ecfe9ecb85219030813
SandeepHooda <sonu.hooda@gmail.com>
2018-09-02 19:25:18 +0530

    optimized search

--- java_search/src/search/Search.java
+++ java_search/src/search/Search.java
@@ -41,14 +41,14 @@ public class Search extends HttpServlet {
 		SearchResult searchResult = new SearchResult();
 		if (query == null) {
 			root = SearchEngine.getRootFolderpopulated(SearchEngine.getAllFiles());
-			searchResult.setExecutionTime(System.currentTimeMillis() - start);
+			searchResult.setSearchTime(System.currentTimeMillis() - start);
 			searchResult.setMatchedFilesCount(SearchEngine.getAllFiles().size());
 		}else {
 			QueryParser queryParser = new QueryParser();
 			String[] keyWords = query.toLowerCase().split(" ");
 			//List<File> filteredFiles = queryParser.getFilteredFiles(keyWords, SearchEngine.getAllFiles());
 			List<File> filteredFiles = queryParser.getFilteredFiles(keyWords, SearchEngine.getKeyWordToFile());
-			searchResult.setExecutionTime(System.currentTimeMillis() - start);
+			searchResult.setSearchTime(System.currentTimeMillis() - start);
 			if (filteredFiles.size()>0) {
 				root = SearchEngine.getRootFolderpopulated(filteredFiles);
 				searchResult.setMatchedFilesCount(filteredFiles.size());
--- java_search/src/search/SearchResult.java
+++ java_search/src/search/SearchResult.java
@@ -8,7 +8,7 @@ public class SearchResult {
 	@Expose()
 	private int matchedFilesCount;
 	@Expose()
-	private long executionTime;
+	private long searchTime;
 	@Expose()
 	private Folder searchResult;
 	public int getMatchedFilesCount() {
@@ -17,17 +17,18 @@ public class SearchResult {
 	public void setMatchedFilesCount(int matchedFilesCount) {
 		this.matchedFilesCount = matchedFilesCount;
 	}
-	public long getExecutionTime() {
-		return executionTime;
-	}
-	public void setExecutionTime(long executionTime) {
-		this.executionTime = executionTime;
-	}
+	
 	public Folder getSearchResult() {
 		return searchResult;
 	}
 	public void setSearchResult(Folder searchResult) {
 		this.searchResult = searchResult;
+	}
+	public long getSearchTime() {
+		return searchTime;
+	}
+	public void setSearchTime(long searchTime) {
+		this.searchTime = searchTime;
 	}
 	
 	
